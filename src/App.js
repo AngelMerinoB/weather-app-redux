1953bec140c508ed3f304427d03750af4e211129
Angel Merino <angel.merino.bernal@hotmail.com>
2019-03-12 09:02:40 -0600

    Version 2.0 adding Redux to substitute bubbling methods for data transfer

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import AppBar from '@material-ui/core/AppBar';
 import Typography from '@material-ui/core/Typography';
 import Toolbar from '@material-ui/core/Toolbar';
 import { Grid, Col, Row } from 'react-flexbox-grid';
-import LocationList from './components/LocationList';
-import ForecastExtended from './components/ForecastExtended';
+import LocationListContainer from './containers/LocationListContainer';
+import ForecastExtendedContainer from './containers/ForecastExtendedContainer';
 import './App.css';
 
 const cities = [
@@ -19,17 +19,8 @@ const cities = [
 ];
 
 class App extends Component {
-
-constructor() {
-  super();
-  this.state = { city: null };
-}
-  handleSelectedLocation = city => {
-    this.setState({ city });
-    console.log(`handleSelectedLocation ${city}`);
-  }
+  
   render() {
-    const { city } = this.state;
     return (
       <Grid>
         <Row>
@@ -43,17 +34,12 @@ constructor() {
         </Row>
         <Row>
           <Col xs={12} md={6}>
-            <LocationList cities= {cities}
-              onSelectedLocation ={this.handleSelectedLocation}>
-            </LocationList>
+            <LocationListContainer cities= {cities}></LocationListContainer>
           </Col>
           <Col xs={12} md={6}>
             <Paper elevation={4}>
               <div className="details">
-                {
-                  city &&
-                    <ForecastExtended city={ city }></ForecastExtended>
-                  }
+                <ForecastExtendedContainer></ForecastExtendedContainer>
               </div>
             </Paper>
           </Col>
